53bee4222d864e3fffbdfbe2a6b31bf84b702025
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2026-05-09 21:33:43 -0300

    feat(products): replace dynamic product fetching with static product list

--- src/services/products.js
+++ src/services/products.js
@@ -1,17 +1,34 @@
-const { getShopeeProducts } = require("./scraper");
+const products = [
+    {
+        title: "Fone Bluetooth TWS",
+        price: "R$49,90",
+        link: "https://shopee.com.br/",
+    },
+    {
+        title: "Smartwatch Ultra",
+        price: "R$129,90",
+        link: "https://shopee.com.br/",
+    },
+    {
+        title: "Caixa de Som JBL",
+        price: "R$199,90",
+        link: "https://shopee.com.br/",
+    },
+    {
+        title: "Teclado Mecânico Gamer",
+        price: "R$159,90",
+        link: "https://shopee.com.br/",
+    },
+    {
+        title: "Mouse Gamer RGB",
+        price: "R$89,90",
+        link: "https://shopee.com.br/",
+    },
+];
 
 async function getRandomProduct() {
-    const products = await getShopeeProducts();
-
-    if (!products || products.length === 0) {
-        return {
-            title: "Produto indisponível",
-            price: "-",
-            link: "#",
-        };
-    }
-
     const index = Math.floor(Math.random() * products.length);
+
     return products[index];
 }
 
